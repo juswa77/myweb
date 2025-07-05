8f0b7a27067203fb1a3ac5e8e9b8c1dd251eca73
juswa77 <josheustaquio77@gmail.com>
2025-07-05 16:56:44 +0800

    carousel

--- script.js
+++ script.js
@@ -277,4 +277,21 @@ spotifyToggle.addEventListener('click', () => {
 
 
 
+let currentSlide = 0;
+const slides = document.querySelectorAll(".carousel-images img");
+const carouselImages = document.querySelector(".carousel-images");
+const totalSlides = slides.length;
 
+document.querySelector(".next").addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  updateCarousel();
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  updateCarousel();
+});
+
+function updateCarousel() {
+  carouselImages.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
